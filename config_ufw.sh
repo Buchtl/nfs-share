e4214f491199b6f87b5c02fda9b10aab1a5af08c
chris <chris@vm>
2025-07-19 17:14:16 +0000

    allow specific ip

--- config_ufw.sh
+++ config_ufw.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+
+ALLOWED_IP=$1
+
 sudo ufw deny 111/tcp
 sudo ufw deny 111/udp
 sudo ufw deny 2049/tcp
 sudo ufw deny 2049/udp
 sudo ufw deny 20048/tcp
 sudo ufw deny 20048/udp
+
+sudo ufw allow from $ALLOWED_IP to any port 111 proto tcp
+sudo ufw allow from $ALLOWED_IP to any port 111 proto udp
+sudo ufw allow from $ALLOWED_IP to any port 2049 proto tcp
+sudo ufw allow from $ALLOWED_IP to any port 2049 proto udp
+sudo ufw allow from $ALLOWED_IP to any port 20048 proto tcp
+sudo ufw allow from $ALLOWED_IP to any port 20048 proto udp
 
 sudo ufw reload
 
